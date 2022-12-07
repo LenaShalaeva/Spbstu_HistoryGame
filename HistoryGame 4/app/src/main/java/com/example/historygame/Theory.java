805a7edcc5f70b0b1f1c18fa790c8e424e528ce5
tronyaginaa <tronyaginaa@gmail.com>
2022-12-07 23:40:33 +0300

    edit theory activities

--- HistoryGame 4/app/src/main/java/com/example/historygame/Theory.java
+++ HistoryGame 4/app/src/main/java/com/example/historygame/Theory.java
@@ -31,10 +31,8 @@ public class Theory extends AppCompatActivity {
                         startActivity(new Intent(getApplicationContext(), Profile.class));
                         overridePendingTransition(0, 0);
                         return true;
-
                     case R.id.theory:
                         return true;
-
                     case R.id.games:
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         overridePendingTransition(0, 0);
@@ -50,11 +48,57 @@ public class Theory extends AppCompatActivity {
             public void onClick(View v) {
                 try{
                     Intent intent=new Intent(Theory.this,Theory_Ancient.class);
-                    startActivity(intent); finish();
+                    intent.putExtra("header","Древний Казахстан");
+                    intent.putExtra("century","1");
+                    startActivity(intent);
                 }catch(Exception e) {
                 }
             }
         });
+
+        TextView textView2=(TextView)findViewById(R.id.textViewG2);
+        textView2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent=new Intent(Theory.this,Theory_Ancient.class);
+                    intent.putExtra("header","Средний век");
+                    intent.putExtra("century","2");
+                    startActivity(intent);
+                }catch(Exception e) {
+                }
+            }
+        });
+
+        TextView textView3=(TextView)findViewById(R.id.textViewG3);
+        textView3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent=new Intent(Theory.this,Theory_Ancient.class);
+                    intent.putExtra("header","Новое время");
+                    intent.putExtra("century","3");
+                    startActivity(intent);
+                }catch(Exception e) {
+                }
+            }
+        });
+
+        TextView textView4=(TextView)findViewById(R.id.textViewG4);
+        textView4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent=new Intent(Theory.this,Theory_Ancient.class);
+                    intent.putExtra("header","Новейшее время");
+                    intent.putExtra("century","4");
+                    startActivity(intent);
+                }catch(Exception e) {
+                }
+            }
+        });
+
+
     }
 
 }
--- HistoryGame 4/app/src/main/java/com/example/historygame/Theory_Ancient.java
+++ HistoryGame 4/app/src/main/java/com/example/historygame/Theory_Ancient.java
@@ -1,8 +1,15 @@
 package com.example.historygame;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +17,120 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
 public class Theory_Ancient extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    TextView header, centuryHeader, theory;
+    ScrollView scroll;
+    String centuryName;
+    String century;
+    ArrayList<String> headers = new ArrayList<String>();
+    ArrayList<String> texts = new ArrayList<String>();
+    int num = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theory_ancient);
 
+        Bundle arguments = getIntent().getExtras();
+        centuryName = arguments.get("header").toString();
+        century = arguments.get("century").toString();
 
+
+        centuryHeader = findViewById(R.id.textView);
+        centuryHeader.setText(centuryName);
+        header = findViewById(R.id.textView3);
+        theory = findViewById(R.id.textView4);
+        scroll = (ScrollView) findViewById(R.id.scroll_view);
+
+        Database db = new Database();
+        centuryHeader.setText(centuryName);
+        Connection connection = db.conclass();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT header, theory_text FROM theory_table WHERE century ='" + century + "'");
+            String headerText, theoryText;
+            while (resultSet.next()) {
+                headerText = resultSet.getString("header");
+                theoryText = resultSet.getString("theory_text");
+                headers.add(headerText);
+                texts.add(theoryText);
+            }
+        } catch (Exception e) {
+            Log.e("Error: ", e.getMessage());
+        }
+        header.setText(headers.get(num));
+        theory.setText(texts.get(num));
+
+        Button button_back = (Button) findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (num == 1){
+                    try {
+                    Intent intent = new Intent(Theory_Ancient.this, Theory.class);
+                    startActivity(intent);
+                    finish();
+                    } catch (Exception e) {
+                    }
+                }
+                else{
+                    num--;
+                    scroll.scrollTo(0,0);
+                    header.setText(headers.get(num));
+                    theory.setText(texts.get(num));
+                }
+            }
+        });
+
+        Button button_forward = (Button) findViewById(R.id.button_forward);
+        button_forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num++;
+                scroll.scrollTo(0,0);
+                header.setText(headers.get(num));
+                theory.setText(texts.get(num));
+            }
+        });
+    }
+
+    class Task extends AsyncTask<Void, Void, Void> {
+        String headerText, theoryText;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Database db = new Database();
+            Connection connection = db.conclass();
+            int n = 0;
+            try {
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT header, theory_text FROM theory_table WHERE century ='" + century + "'");
+                while (resultSet.next() && n < num) {
+                    headerText = resultSet.getString("header");
+                    theoryText = resultSet.getString("theory_text");
+                    headers.add(headerText);
+                }
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            header.setText(headerText);
+            theory.setText(theoryText);
+            centuryHeader.setText(centuryName);
+            super.onPostExecute(unused);
+        }
     }
 
     @Override
@@ -30,4 +141,5 @@ public class Theory_Ancient extends AppCompatActivity {
         }catch(Exception e) {
         }
     }
+
 }
